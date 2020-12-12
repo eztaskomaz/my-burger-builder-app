17ff3886b47567817ae279e907438dc71c59cad3
eztaskomaz <eztaskomaz@gmail.com>
2020-12-12 17:39:19 +0300

    cancel/continue buttons

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -17,8 +18,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout?</p>
-            <button>CANCEL</button>
-            <button>CONTINUE</button>
+            <Button clicked={props.purchaseCanceled} btnType='Danger'>CANCEL</Button>
+            <Button clicked={props.purchaseContinued} btnType='Success'>CONTINUE</Button>
         </Aux>
     )
 }
