82b850d08b43dd199184576b1e6bbf993c9be038
eztaskomaz <eztaskomaz@gmail.com>
2020-12-27 15:25:14 +0300

    improving the app

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,26 +1,29 @@
 import React from 'react';
+
 import Logo from '../../Logo/Logo';
-import NavigationItems from "../NavigationItems/NavigationItems";
+import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
-import BackDrop from '../../UI/Backdrop/Backdrop';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux/Aux';
 
 const sideDrawer = (props) => {
-    let attachedClasses = [classes.SideDrawer, classes.Closed];
-    if(props.open) {
-        attachedClasses = [classes.SideDrawer, classes.Open]
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
     }
     return (
         <Aux>
-            <BackDrop show={props.open} clicked={props.closed}/>
+            <Backdrop show={props.open} clicked={props.closed}/>
             <div className={attachedClasses.join(' ')}>
-                <Logo height='11%' margin-bottom='32px'/>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
                 <nav>
-                    <NavigationItems isAuthenticated={props.isAuth}/>
+                    <NavigationItems/>
                 </nav>
             </div>
         </Aux>
     );
-}
+};
 
 export default sideDrawer;
